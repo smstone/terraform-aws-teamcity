363077aa59b0d8b529666e46c209c748a46c5cb9
Steven Stone <steven.stone@clickhouse.com>
2025-03-28 15:56:02 -0400

    Attach instance to tg

--- aws_alb.service_alb.tf
+++ aws_alb.service_alb.tf
@@ -63,3 +63,9 @@ resource "aws_lb_target_group" "alb_target_group" {
 
   tags = var.tags
 }
+
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.teamcity.id
+  port             = 8111
+}
