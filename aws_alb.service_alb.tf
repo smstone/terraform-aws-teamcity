379035dd3f87936a2796267057bc4c65248b3776
Steven Stone <steven.stone@clickhouse.com>
2025-03-28 13:37:50 -0400

    Add ALB

---
+++ aws_alb.service_alb.tf
@@ -0,0 +1,63 @@
+resource "aws_lb" "alb" {
+  internal           = var.alb_internal
+  load_balancer_type = "application"
+  subnets            = var.alb_internal ? var.private_subnets : var.public_subnets
+  security_groups    = [aws_security_group.alb.id]
+
+  tags = local.tags
+}
+
+resource "aws_lb_listener" "alb_listener_80" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "alb_listener_443" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = var.alb_acm_arn
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  deregistration_delay = 60
+  port                 = 8111
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+
+  health_check {
+    interval          = 30
+    port              = 8111
+    protocol          = "HTTP"
+    healthy_threshold = 3
+    path              = "/healthCheck/healthy"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = local.tags
+}
---
+++ aws_security_group.alb.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "alb" {
+  name        = "teamcity_alb"
+  vpc_id      = var.vpc_id
+  description = "Protects ALB access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "http access"
+    # tfsec:ignore:AWS008
+    cidr_blocks = var.alb_allowlist
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "SSL access"
+    # tfsec:ignore:AWS008
+    cidr_blocks = var.alb_allowlist
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    description = "Allow outbound"
+    # tfsec:ignore:AWS009
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.common_tags
+}
